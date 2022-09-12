689b9a4186ab5f7a571080fb0f4206476d746e18
Derrick Fleming <derrick.fleming7@gmail.com>
2022-09-12 22:48:16 +0000

    Put the form's input values into a new object. #1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+var $codeJournal = document.querySelector('#code-journal');
 var $placeHolderImage = document.querySelector('.placeholder-image');
 var $photoUrlInput = document.querySelector("input[name='photoUrl']");
 
@@ -5,8 +6,20 @@ $photoUrlInput.addEventListener('input', photoUpload);
 
 function photoUpload(event) {
   if (event.target.value) {
-    $placeHolderImage.setAttribute('src', event.target.value);
+    $placeHolderImage.setAttribute('src', $photoUrlInput.value);
   } else {
     $placeHolderImage.setAttribute('src', 'images/placeholder-image-square.jpg');
   }
+}
+
+$codeJournal.addEventListener('submit', saveEntries);
+
+function saveEntries(event) {
+  event.preventDefault();
+  var formInputValues = {
+    title: $codeJournal.elements.title.value,
+    photoUrl: $codeJournal.elements.photoUrl.value,
+    textArea: $codeJournal.elements.notes.value
+  };
+  return formInputValues;
 }
