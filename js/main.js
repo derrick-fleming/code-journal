09430744d14e165ac0ca4fb6a7008ffb5dcae98b
Derrick Fleming <derrick.fleming7@gmail.com>
2022-09-13 22:28:43 +0000

    Ensured submitting new journal entry automatically shows 'entries' view without reloading the page and ensured refreshing the page shows same view as before refreshing. #2

--- js/main.js
+++ js/main.js
@@ -26,6 +26,9 @@ function saveEntries(event) {
 
   $placeHolderImage.setAttribute('src', 'images/placeholder-image-square.jpg');
   $codeJournal.reset();
+
+  $codeJournal.className = 'row hidden';
+  $entries.className = '';
 }
 
 $codeJournal.addEventListener('submit', saveEntries);
@@ -34,15 +37,62 @@ var $entriesLink = document.querySelector('.nav-link');
 var $newEntry = document.querySelector('.button.entry');
 
 $entriesLink.addEventListener('click', allEntriesView);
+$newEntry.addEventListener('click', newEntryView);
 
 function allEntriesView(event) {
   $codeJournal.className = 'row hidden';
   $entries.className = '';
 }
 
-$newEntry.addEventListener('click', newEntryView);
-
 function newEntryView(event) {
   $codeJournal.className = 'row';
   $entries.className = 'hidden';
 }
+
+function entryDomTree(entry) {
+  var $li = document.createElement('li');
+  $li.className = 'row';
+
+  var $imageDiv = document.createElement('div');
+  $imageDiv.className = 'column-half';
+
+  var $image = document.createElement('img');
+  $image.className = 'placeholder-image';
+  $image.setAttribute('src', entry.photoUrl);
+  $image.setAttribute('alt', entry.title);
+
+  $imageDiv.appendChild($image);
+  $li.appendChild($imageDiv);
+
+  var $textDiv = document.createElement('div');
+  $textDiv.className = 'column-half journal-entry';
+
+  var $entryHeading = document.createElement('h2');
+  $entryHeading.className = 'journal-entry-title';
+  $entryHeading.textContent = entry.title;
+
+  var $entryText = document.createElement('p');
+  $entryText.textContent = entry.textArea;
+
+  $textDiv.appendChild($entryHeading);
+  $textDiv.appendChild($entryText);
+  $li.appendChild($textDiv);
+
+  return $li;
+
+}
+
+var $ul = document.querySelector('ul');
+document.addEventListener('DOMContentLoaded', appendJournal);
+
+function appendJournal(event) {
+  for (var i = 0; i < data.entries.length; i++) {
+    $ul.appendChild(entryDomTree(data.entries[i]));
+  }
+}
+
+function prependJournal(event) {
+  $ul.prepend(entryDomTree(data.entries[0]));
+}
+
+$codeJournal.addEventListener('submit', prependJournal);
