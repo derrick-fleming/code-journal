74ad241598bc5083fa4638cec3bef4127818998b
Derrick Fleming <derrick.fleming7@gmail.com>
2022-09-13 19:09:33 +0000

    Defined a function that takes a single journal entry object and returns a DOM tree that matches one of the example entries. #2

--- js/main.js
+++ js/main.js
@@ -28,3 +28,38 @@ function saveEntries(event) {
 }
 
 $codeJournal.addEventListener('submit', saveEntries);
+
+function entryDomTree(entry) {
+  var $li = document.createElement('li');
+  $li.className = 'row';
+
+  var $imageDiv = document.createElement('div');
+  $imageDiv.className = 'column-half';
+
+  var $image = document.createElement('img');
+  $image.className = 'placeholder-image';
+  $image.setAttribute('src', data.entries[0].photoUrl);
+  $image.setAttribute('alt', data.entries[0].title);
+
+  $imageDiv.appendChild($image);
+  $li.appendChild($imageDiv);
+
+  var $textDiv = document.createElement('div');
+  $textDiv.className = 'column-half journal-entry';
+
+  var $entryHeading = document.createElement('h2');
+  $entryHeading.className = 'journal-entry-title';
+  $entryHeading.textContent = data.entries[0].title;
+
+  var $entryText = document.createElement('p');
+  $entryText.textContent = data.entries[0].textArea;
+
+  $textDiv.appendChild($entryHeading);
+  $textDiv.appendChild($entryText);
+  $li.appendChild($textDiv);
+
+  return $li;
+
+}
+
+entryDomTree(data);
